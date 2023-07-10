4c6e4ff5287b44a50bcd5de94d4c16b41a509bff
le0nklcpp <le0nklcpp@yandex.ru>
2023-07-11 00:46:20 +0300

    Fix debrisEnt constructor, create_debris command, debris_remove_time

--- src/game/debris.cpp
+++ src/game/debris.cpp
@@ -1,12 +1,13 @@
 #include "debris.h"
-VARP(debris_remove_time,0,30000,0); // 0 = do not remove
+VARP(debris_remove_time,0,0,300000); // 0 = do not remove
 
 debrisEnt::debrisEnt(vec pos,vec deviation,vec dir,vec v,string model):fpsEntity()
 {
- o = vec(pos).add(game::randomspread(deviation));
+ setpos(vec(pos).add(game::randomspread(deviation)));
  vel = game::randomspread(v);
  setangle(dir.x,dir.y,dir.z);
  setmodel(model);
+ movable = true;
  if(!debris_remove_time)state = CS_ALIVE;
  nextthink = lastmillis + debris_remove_time;
  type = E_FRAGMENT;
@@ -19,13 +20,13 @@ inline void debrisEnt::move()
   setangle(sp.x,sp.y,sp.z);
  }
 }
-void add_debris(char*p,char*e,char*d,char*v,char*s,int count)
+void add_debris(char*p,char*e,char*d,char*v,char*s,int* count)
 {
  int pos[3],dev[3],dir[3],vel[3];
  parsearray(p,pos);
  parsearray(e,dev);
  parsearray(d,dir);
  parsearray(v,vel);
- loopi(count)game::fpsents.add(new debrisEnt(vec(pos),vec(dev),vec(dir),vec(vel),s));
+ loopi(*count)game::fpsents.add(new debrisEnt(vec(pos),vec(dev),vec(dir),vec(vel),s));
 }
 COMMANDN(create_debris,add_debris,"sssssi");
