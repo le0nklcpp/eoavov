741645a3325391baa1123f15225c9ddbf76285fd
le0nklcpp <le0nklcpp@yandex.ru>
2025-12-02 19:01:55 +0300

    Fix spaces

--- src/game/debris.cpp
+++ src/game/debris.cpp
@@ -5,41 +5,41 @@ VARP(debris_kick_normal,0,90,2000);
 
 debrisEnt::debrisEnt(vec pos,vec deviation,vec dir,vec v,string model):fpsEntity()
 {
- setpos(vec(pos).add(game::randomspread(deviation)));
- vel = game::randomspread(v);
- setangle(dir.x,dir.y,dir.z);
- setmodel(model);
- movable = true;
- if(!debris_remove_time)state = CS_ALIVE;
- nextthink = lastmillis + debris_remove_time;
- type = E_FRAGMENT;
+    setpos(vec(pos).add(game::randomspread(deviation)));
+    vel = game::randomspread(v);
+    setangle(dir.x,dir.y,dir.z);
+    setmodel(model);
+    movable = true;
+    if(!debris_remove_time)state = CS_ALIVE;
+    nextthink = lastmillis + debris_remove_time;
+    type = E_FRAGMENT;
 }
 inline void debrisEnt::move()
 {
- if(bounce(this,0.6,0.5,1)&&collideplayer) // bounce returns true when it collides
- {
-  vel.add(kickvec.rotate_around_z(sincosmod360(collideplayer->yaw)));
- }
- if(!onfloor())
- {
-  vec oa(yaw,pitch,roll);
-  vec sp = game::randomspread(vec(30,30,30));
-  setangle(oa.x+sp.x,oa.y+sp.y,oa.z+sp.z);
- }
- else
- {
-   pitch = 90;
-   roll = 0;
- }
+    if(bounce(this,0.6,0.5,1)&&collideplayer) // bounce returns true when it collides
+    {
+        vel.add(kickvec.rotate_around_z(sincosmod360(collideplayer->yaw)));
+    }
+    if(!onfloor())
+    {
+        vec oa(yaw,pitch,roll);
+        vec sp = game::randomspread(vec(30,30,30));
+        setangle(oa.x+sp.x,oa.y+sp.y,oa.z+sp.z);
+    }
+    else
+    {
+        pitch = 90;
+        roll = 0;
+    }
 }
 void add_debris(char*p,char*e,char*d,char*v,char*s,int* count)
 {
- if(!isconnected())return;
- int pos[3],dev[3],dir[3],vel[3];
- parsearray(p,pos);
- parsearray(e,dev);
- parsearray(d,dir);
- parsearray(v,vel);
- loopi(*count)game::fpsents.add(new debrisEnt(vec(pos),vec(dev),vec(dir),vec(vel),s));
+    if(!isconnected())return;
+    int pos[3],dev[3],dir[3],vel[3];
+    parsearray(p,pos);
+    parsearray(e,dev);
+    parsearray(d,dir);
+    parsearray(v,vel);
+    loopi(*count)game::fpsents.add(new debrisEnt(vec(pos),vec(dev),vec(dir),vec(vel),s));
 }
 COMMANDN(create_debris,add_debris,"sssssi");
--- src/game/player.cpp
+++ src/game/player.cpp
@@ -20,130 +20,118 @@ VAR(ENABLE_DOUBLEJUMP,1,0,1);
 
 void fixcarrydist()
 {
- max_carry_dist = max(min_carry_dist,max_carry_dist);
+    max_carry_dist = max(min_carry_dist,max_carry_dist);
 }
 void playerEnt::reset()
 {
- lastnode = -1;
- doublejump = false;
- vmodel[0]='\0';
- fpsEntity::reset();
- RPGObject::reset();
- state = CS_ALIVE;
- maxspeed = PLAYER_DEFAULT_SPEED;
- nextinteracttime = lastdamagetime = lastvanimtime = 0;
- movable = true;
- light = false;
- carries = NULL;
- dragging = NULL;
+    lastnode = -1;
+    doublejump = false;
+    vmodel[0]='\0';
+    fpsEntity::reset();
+    RPGObject::reset();
+    state = CS_ALIVE;
+    maxspeed = PLAYER_DEFAULT_SPEED;
+    nextinteracttime = lastdamagetime = lastvanimtime = 0;
+    movable = true;
+    light = false;
+    carries = NULL;
+    dragging = NULL;
 }
 void playerEnt::setvmodel(const char*path,int anim,int animtime)
 {
- copystring(vmodel,path);
- vanim = (ANIM_GAMESPECIFIC + max(anim,0)) | ANIM_LOOP;
- lastvanimtime = lastmillis-animtime;
+    copystring(vmodel,path);
+    vanim = (ANIM_GAMESPECIFIC + max(anim,0)) | ANIM_LOOP;
+    lastvanimtime = lastmillis-animtime;
 }
 void playerEnt::removeitem(int index)
 {
-  RPGObject::removeitem(index);
-  if(hands==NULL)setvmodel("\0",0,0); // 1 excess operation
-  /*
-  invItem*item = inv.getitem(index);
-  if(item==hands)
-  {
-   hands=NULL;
-   setvmodel("\0",0,0);
-  }
-  if(item==holster)holster=NULL;
-  inv.removeitem(index);
-
-  Maybe it is worth moving RPGObject structure to playerEnt
-  */
+    RPGObject::removeitem(index);
+    if(hands==NULL)setvmodel("\0",0,0);
 }
 void playerEnt::dropitem(int index) // well, it could've been worse
 {
- invItem*item = inv.getitem(index);
- int i;
- if(!item)return;
- RPGItemEnt * e = (RPGItemEnt*)game::fpsents.add(new RPGItemEnt(item));
- for(i=min_carry_dist;i<max_carry_dist;i++)
-  {
-   e->setpos(vec(o).add(vec(cameradir()).mul(i)));
-   if(!collide(e)&&!collideinside)break;
-  }
- if(i==max_carry_dist) // throw it under player feet
-  {
-  vec oldpos = o;
-  e->setpos(feetpos().add(vec(0,0,e->eyeheight+e->aboveeye)));
-  setpos(vec(o).add(vec(0,0,e->eyeheight+e->aboveeye+1.f)));
-  if((collide(this)||collideinside)||(collide(e)||collideinside)) // collideinside resets
-   {
-    setpos(oldpos);
-    game::fpsremove(e);
-    return;
-   }
-  }
- else e->vel = vec(cameradir()).mul(THROW_FORCE).add(vel);
- e->physstate = PHYS_FALL;
- removeitem(index);
+    invItem*item = inv.getitem(index);
+    int i;
+    if(!item)return;
+    RPGItemEnt * e = (RPGItemEnt*)game::fpsents.add(new RPGItemEnt(item));
+    for(i=min_carry_dist;i<max_carry_dist;i++)
+    {
+        e->setpos(vec(o).add(vec(cameradir()).mul(i)));
+       if(!collide(e)&&!collideinside)break;
+    }
+    if(i==max_carry_dist) // throw it under player feet
+    {
+        vec oldpos = o;
+        e->setpos(feetpos().add(vec(0,0,e->eyeheight+e->aboveeye)));
+        setpos(vec(o).add(vec(0,0,e->eyeheight+e->aboveeye+1.f)));
+        if((collide(this)||collideinside)||(collide(e)||collideinside)) // collideinside resets
+        {
+            setpos(oldpos);
+            game::fpsremove(e);
+            return;
+        }
+    }
+    else e->vel = vec(cameradir()).mul(THROW_FORCE).add(vel);
+    e->physstate = PHYS_FALL;
+    removeitem(index);
 }
 inline void playerEnt::moveitem()
 {
- if(carries)
- {
-  vec oldpos = carries->o;
-  int i=max_carry_dist;
-  while(i>=min_carry_dist)
-   {
-    carries->o = vec(o).add(vec(cameradir()).mul(i));
-    if((!collide(carries)&&!collideinside))
-     {
-      carries->resetinterp();
-      return;
-     }
-    i--;
-   }
-  carries->o = oldpos;
-  dropent();
- }
- if(dragging)
- {
-  vec ppos = feetpos();
-  if(ppos.dist(dragging->o) < min_dragging_dist)return;
-  if(ppos.dist(dragging->o) > max_dragging_dist || (o.z - dragging->o.z) > min_dragging_dist)
-  {
-   stopdragging();
-   return;
-  }
-  bool upped = false;
-  if(dragging->onfloor())
-  {
-   dragging->o.addz(1.0f);
-   upped = true;
-  }
-  dragging->turnto(ppos);
-  dragging->pitch = 0;
-  vec d = dragging->cameradir();
-  if(upped)dragging->o.addz(-1.0f);
-  while(ppos.dist2(dragging->o) > min_dragging_dist)
-  {
-   if(collide(dragging, d, 0.0f, true, true))break;
-   dragging->o.add(d);
-  }
-  dragging->resetinterp();
- }
+    if(carries)
+    {
+        vec oldpos = carries->o;
+        int i=max_carry_dist;
+        while(i>=min_carry_dist)
+        {
+            carries->o = vec(o).add(vec(cameradir()).mul(i));
+            if((!collide(carries)&&!collideinside))
+            {
+                carries->resetinterp();
+                return;
+            }
+        i--;
+        }
+    carries->o = oldpos;
+    dropent();
+    }
+    if(dragging)
+    {
+        vec ppos = feetpos();
+        if(ppos.dist(dragging->o) < min_dragging_dist)return;
+        if(ppos.dist(dragging->o) > max_dragging_dist || (o.z - dragging->o.z) > min_dragging_dist)
+        {
+            stopdragging();
+            return;
+        }
+        bool upped = false;
+        if(dragging->onfloor())
+        {
+            dragging->o.addz(1.0f);
+            upped = true;
+        }
+        dragging->turnto(ppos);
+        dragging->pitch = 0;
+        vec d = dragging->cameradir();
+        if(upped)dragging->o.addz(-1.0f);
+        while(ppos.dist2(dragging->o) > min_dragging_dist)
+        {
+            if(collide(dragging, d, 0.0f, true, true))break;
+            dragging->o.add(d);
+        }
+        dragging->resetinterp();
+    }
 }
 void playerEnt::move()
 {
- if(!movable)return;
- if(lock_movement)player1->stopmoving();
- crouchplayer(this, 1, true);
- moveplayer(this, 1, true);
- moveitem();
+    if(!movable)return;
+    if(lock_movement)player1->stopmoving();
+    crouchplayer(this, 1, true);
+    moveplayer(this, 1, true);
+    moveitem();
 }
 void playerEnt::falldamage(int ftime)
 {
- doublejump = false;
+    doublejump = false;
 }
 void playerEnt::stopdragging()
 {
@@ -152,113 +140,113 @@ void playerEnt::stopdragging()
 }
 void playerEnt::dropent()
 {
- if(carries)
-  {
-  carries->resetinterp();
-  if(carries->type==E_MOVABLE)
-   {
-   propEnt*cast = (propEnt*)carries;
-   cast->dropped();
-   }
-  }
- carries = NULL;
+    if(carries)
+    {
+        carries->resetinterp();
+        if(carries->type==E_MOVABLE)
+        {
+        propEnt*cast = (propEnt*)carries;
+        cast->dropped();
+        }
+    }
+    carries = NULL;
 }
 void playerEnt::grabent(fpsEntity*e)
 {
- carries = e;
+    carries = e;
 }
 void playerEnt::killed(fpsEntity*killer)
 {
- dropent();
- state = CS_DEAD;
- roll = 0;
- yaw = pitch = 90;
- nextthink = lastmillis + LOSE_DELAY;
- game::cubeevent("player_killed");
+    dropent();
+    state = CS_DEAD;
+    roll = 0;
+    yaw = pitch = 90;
+    nextthink = lastmillis + LOSE_DELAY;
+    game::cubeevent("player_killed");
 }
 void playerEnt::quickswitch()
 {
- if(!holster)return;
- invItem*buf = hands;
- hands = holster;
- holster = buf;
+    if(!holster)return;
+    invItem*buf = hands;
+    hands = holster;
+    holster = buf;
 }
 void playerEnt::think()
 {
- if(state!=CS_DEAD)move();
- int tilt = camrot;
- if(k_left)tilt -= cameratilt;
- else if(k_right)tilt += cameratilt;
- setcamtilt(tilt);
+    if(state!=CS_DEAD)move();
+    int tilt = camrot;
+    if(k_left)tilt -= cameratilt;
+    else if(k_right)tilt += cameratilt;
+    setcamtilt(tilt);
 }
 void playerEnt::attack(bool down)
 {
- if(nextattacktime>lastmillis)return;
- if(carries)
-  {
-  fpsEntity* ent = carries;
-  dropent();
-  ent->vel.add(vec(cameradir()).mul(THROW_FORCE).div(ent->mass));
-  nextattacktime = lastmillis + NEXT_USE_DELAY;
-  }
- else if(hands)
-  {
-   hands->parent->use(this,hands,down);
-  }
+    if(nextattacktime>lastmillis)return;
+    if(carries)
+    {
+        fpsEntity* ent = carries;
+        dropent();
+        ent->vel.add(vec(cameradir()).mul(THROW_FORCE).div(ent->mass));
+        nextattacktime = lastmillis + NEXT_USE_DELAY;
+    }
+    else if(hands)
+    {
+        hands->parent->use(this,hands,down);
+    }
 }
 void playerEnt::attack2(bool down)
 {
- if(crouching&&!down)
-  {
-   fpsEntity*ent = game::rayfpsent(o,cameradir(),MAX_INTERACT_DIST,false);
-   if(ent&&ent->draggable())
+    if(crouching&&!down)
     {
-     if(dragging != ent)dragging = ent;
-     else stopdragging();
-     game::recountspeed(togglewalk);
+        fpsEntity*ent = game::rayfpsent(o,cameradir(),MAX_INTERACT_DIST,false);
+        if(ent&&ent->draggable())
+        {
+            if(dragging != ent)dragging = ent;
+            else stopdragging();
+            game::recountspeed(togglewalk);
+        }
     }
- }
 }
 bool playerEnt::setev(int attr,const char*val)
 {
- if(!fpsEntity::setev(attr,val)&&val)
-  {
-   #define setv(a) a = lastmillis + atoi(val)
-   switch(attr)
+    if(!fpsEntity::setev(attr,val)&&val)
     {
-     case(EV_NEXTATTACKTIME):setv(nextattacktime);break;
-     case(EV_NEXTINTERACTTIME):setv(nextinteracttime);break;
-     default:return false;break;
+        #define setv(a) a = lastmillis + atoi(val)
+        switch(attr)
+        {
+        case(EV_NEXTATTACKTIME):setv(nextattacktime);break;
+        case(EV_NEXTINTERACTTIME):setv(nextinteracttime);break;
+        default:return false;break;
+        }
     }
-  }
  return true;
 }
 bool playerEnt::getev(int attr)
 {
- #define getv(a) intret(a)
- if(!fpsEntity::getev(attr))
-  {
-   switch(attr)
+    #define getv(a) intret(a)
+    if(!fpsEntity::getev(attr))
     {
-     case(EV_NEXTATTACKTIME):getv(nextattacktime);break;
-     case(EV_NEXTINTERACTTIME):getv(nextinteracttime);break;
-     default:return false;break;
+        switch(attr)
+        {
+        case(EV_NEXTATTACKTIME):getv(nextattacktime);break;
+        case(EV_NEXTINTERACTTIME):getv(nextinteracttime);break;
+        default:return false;break;
+        }
     }
-  }
- return true;
+    return true;
 }
 invItem* playerEnt::draw(int index)
 {
- if(index==-1)
-  {
-  hands = NULL;
-  setvmodel("\0",0,0);
-  return NULL;
-  }
- invItem*item = RPGObject::draw(index);
- if(item)
-  {
-   item->parent->draw(this,item);
-  }
- return item;
+    if(index==-1)
+    {
+        hands = NULL;
+        setvmodel("\0",0,0);
+        return NULL;
+    }
+    invItem*item = RPGObject::draw(index);
+    if(item)
+    {
+        item->parent->draw(this,item);
+    }
+    return item;
 }
