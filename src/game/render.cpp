428c14ec75900903372464e54d9924f05ffbb506
le0nklcpp <le0nklcpp@yandex.ru>
2025-12-03 15:28:21 +0300

    Fix spaces

--- src/game/render.cpp
+++ src/game/render.cpp
@@ -26,53 +26,54 @@ namespace game
   }
   void rendergame()
   {
-   renderfpsents();
-   renderplayer();
-   if(showwaypoints)renderwaypoints();
+      renderfpsents();
+      renderplayer();
+      if(showwaypoints)renderwaypoints();
   }
   void renderfpsents()
   {
-   loopv(fpsents)
-    {
-     fpsEntity &e = *fpsents[i];
-     if(!e.visible)continue;
-     int anim = (e.animsequence!=-1?e.animsequence+ANIM_GAMESPECIFIC:ANIM_MAPMODEL)|ANIM_LOOP;
-     rendermodel(e.model,anim,e.feetpos(),e.yaw,e.pitch,e.roll,MDL_CULL_VFC | MDL_CULL_OCCLUDED | MDL_CULL_QUERY,NULL,NULL,e.lastanim?e.lastanim:lastmillis);
-    }
+      loopv(fpsents)
+      {
+          fpsEntity &e = *fpsents[i];
+          if(!e.visible)continue;
+          int anim = (e.animsequence!=-1?e.animsequence+ANIM_GAMESPECIFIC:ANIM_MAPMODEL)|ANIM_LOOP;
+          rendermodel(e.model,anim,e.feetpos(),e.yaw,e.pitch,e.roll,MDL_CULL_VFC | MDL_CULL_OCCLUDED | MDL_CULL_QUERY,NULL,NULL,e.lastanim?e.lastanim:lastmillis);
+      }
   }
   void renderplayer()
   {
+      
   }
   void drawhudmodel()
   {
-    if(!showhud||player1->vmodel[0]=='\0')return; // Nothing to draw
-    vec sway;
-    vecfromyawpitch(player1->yaw, 0, 0, 1, sway);
-    float steps = swaydist/swaystep*M_PI;
-    if(!hudgunsway)sway = player1->o;
-        else
-         {
-     sway.mul(swayside*cosf(steps));
-     sway.z = swayup*(fabs(sinf(steps)) - 1);
-     sway.add(swaydir).add(player1->o);
-         }
+      if(!showhud||player1->vmodel[0]=='\0')return; // Nothing to draw
+      vec sway;
+      vecfromyawpitch(player1->yaw, 0, 0, 1, sway);
+      float steps = swaydist/swaystep*M_PI;
+      if(!hudgunsway)sway = player1->o;
+      else
+      {
+          sway.mul(swayside*cosf(steps));
+          sway.z = swayup*(fabs(sinf(steps)) - 1);
+          sway.add(swaydir).add(player1->o);
+      }
     //modelattach a[2];
     //player1->muzzle = vec(-1, -1, -1);
     //a[0] = modelattach("tag_muzzle", &player1->muzzle);
-    rendermodel(player1->vmodel, player1->vanim, sway, player1->yaw, player1->pitch, 0, MDL_NOBATCH, NULL, NULL/*a*/, player1->lastvanimtime, 0, 1);
-    if(player1->muzzle.x >= 0) player1->muzzle = calcavatarpos(player1->muzzle, 12);
+      rendermodel(player1->vmodel, player1->vanim, sway, player1->yaw, player1->pitch, 0, MDL_NOBATCH, NULL, NULL/*a*/, player1->lastvanimtime, 0, 1);
+      if(player1->muzzle.x >= 0) player1->muzzle = calcavatarpos(player1->muzzle, 12);
   }
   void findanims(const char *pattern, vector<int> &anims)
   {
-     loopi(sizeof(animnames)/sizeof(animnames[0])) if(matchanim(animnames[i], pattern)) anims.add(i);
+      loopi(sizeof(animnames)/sizeof(animnames[0])) if(matchanim(animnames[i], pattern)) anims.add(i);
   }
   void renderavatar()
   {
-    drawhudmodel();
+      drawhudmodel();
   }
   void renderplayerpreview(int model, int color, int team, int weap)
   {
-   return;
+      return;
   }
   int numanims() { return NUMANIMS; }
 
